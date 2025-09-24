269a6cc5745dff0afd87a0fd5d991d4e7ac1e6e9
KingdomAccess <jordan@kingdomaccess.org>
2025-09-24 10:55:29 -0400

    Fix audio prompt text for SAME code menu

--- generate_prompts.sh
+++ generate_prompts.sh
@@ -1,6 +1,6 @@
 # Main menu
 pico2wave -l en-US -w /tmp/same_main_raw.wav \
-"NWS SAME code subscription menu... If you don't know the same codes for your area, please visit weather dot G O V slash N W R slash counties... Press 1 to add a code.. Press 2 to remove a code.. Press 3 to hear a list of your codes......."
+"(...)NWS SAME code subscription menu... If you don't know the same codes for your area, please visit weather dot G O V slash N W R slash counties... Press 1 to add a code.. Press 2 to remove a code.. Press 3 to hear a list of your codes......."
 sox /tmp/same_main_raw.wav -r 16000 -c 1 -b 16 -e signed-integer \
 /var/lib/asterisk/sounds/custom/same-main-menu.wav norm -3
 mv /var/lib/asterisk/sounds/custom/same-main-menu.wav \
